dcded06a5c240570aacbcc4d5ed4c3cb3a970b92
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-11-23 14:00:13 -0500

    fix: fetching guild settings

--- src/lib/util/bot-api.ts
+++ src/lib/util/bot-api.ts
@@ -150,7 +150,7 @@ export async function deleteVoiceRoom(guildId: string, roomId: string) {
 }
 
 export async function createVoiceSpawnRoom(guildId: string, originId: string, options?: { user_limit?: number; can_rename?: boolean; can_lock?: boolean; can_adjust_limit?: boolean }) {
-    const settings = await container.cache.get(`settings_${guildId}`);
+    const settings = await getGuildSettings(guildId);
     const data = await _requestEndpoint<{
         channel_id: string;
         user_limit: number;
@@ -172,7 +172,7 @@ export async function createVoiceSpawnRoom(guildId: string, originId: string, op
 }
 
 export async function modifyVoiceSpawnRoom(guildId: string, originId: string, options: { user_limit?: number; can_rename?: boolean; can_lock?: boolean; can_adjust_limit?: boolean }) {
-    const settings = await container.cache.get(`settings_${guildId}`);
+    const settings = await getGuildSettings(guildId);
     const data = await _requestEndpoint<{
         channel_id: string;
         user_limit: number;
@@ -197,7 +197,7 @@ export async function modifyVoiceSpawnRoom(guildId: string, originId: string, op
 }
 
 export async function deleteVoiceSpawnRoom(guildId: string, originId: string) {
-    const settings = await container.cache.get(`settings_${guildId}`);
+    const settings = await getGuildSettings(guildId);
     const data = _requestEndpoint<{ success: boolean }>({
         path: `/v1/${guildId}/spawn-room/${originId}/delete`,
         method: "DELETE",
@@ -216,7 +216,7 @@ export async function deleteVoiceSpawnRoom(guildId: string, originId: string) {
 }
 
 export async function modifyActivitySettings(guildId: string, type: 'voice' | 'chat', options: { enabled?: boolean; points?: number; cooldown?: number; }) {
-    const settings = await container.cache.get(`settings_${guildId}`);
+    const settings = await getGuildSettings(guildId);
     const data = _requestEndpoint<{ success: boolean }>({
         path: `/v1/${guildId}/activity-tracking/${type}/options`,
         method: "POST",
