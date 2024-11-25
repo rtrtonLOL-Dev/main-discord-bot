5519815dd81846ae855e8df384840c204db23f98
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-11-25 01:32:11 -0500

    refactor: activity tracking task
    instead of deleting the old task and creating a new one, instead we update the data for the existing task.

--- src/listeners/TrackVoiceActivity.ts
+++ src/listeners/TrackVoiceActivity.ts
@@ -7,41 +7,34 @@ import { Events, type VoiceState } from 'discord.js';
     once: false
 })
 export class VoiceRoomCreation extends Listener {
-    private async _getCurrentJobId(memberId: string) {
+    private async _getCurrentTask(memberId: string) {
         const taskList = await this.container.tasks.client.getJobs(['active' ,'delayed' ,'prioritized' ,'waiting' ,'waiting-children']);
         const task = taskList
-            .filter(({ data }) => !Object.keys(data as any).includes('memberId'))
+            .filter(({ data }) => Object.keys(data as any).includes('memberId'))
             .find(({ data }) => {
                 return (data as any).memberId === memberId;
             });
 
         if (!task) return null;
 
-        return task.repeatJobKey;
+        return task;
     }
 
     public override async run(previous: VoiceState, current: VoiceState) {
         if (!current.member || !current.guild) return;
 
         const settings = await this.container.api.getGuildSettings(current.guild.id);
-        const existingId = await this._getCurrentJobId(current.member.id);
+        const existingTask = await this._getCurrentTask(current.member.id);
 
         if (!settings.voice_activity.enabled) {
-            if (existingId) await this.container.tasks.client.removeJobScheduler(existingId);
+            if (existingTask) await this.container.tasks.client.removeJobScheduler(existingTask.repeatJobKey!);
             return;
         }
 
-        // This check is here due to tracking not working if they get moved to a different voice room.
-        // Also, just doesn't really make sense to track the spawn room channels anyways!
-        const rooms = settings.spawn_rooms.map(({ channel_id }) => channel_id);
-        if (rooms.includes(current.channelId!)) return;
-
         if (!current.channelId) {
-            if (existingId) await this.container.tasks.client.removeJobScheduler(existingId);
+            if (existingTask) await this.container.tasks.client.removeJobScheduler(existingTask.repeatJobKey!);
         }
-        else if (previous.channelId !== current.channelId) {
-            if (existingId) await this.container.tasks.client.removeJobScheduler(existingId);
-            
+        else if (previous.channelId === null && current.channelId !== null) {
             await this.container.tasks.create(
                 {
                     name: 'IncrementVoiceActivity',
@@ -53,6 +46,11 @@ export class VoiceRoomCreation extends Listener {
                     customJobOptions: { jobId: current.member.id }
                 }
             );
+        }
+        else if (previous.channelId !== current.channelId) {
+            if (existingTask) {
+                existingTask.updateData({ guildId: current.guild.id, channelId: current.channelId, memberId: current.member.id });
+            }
         }
 
         return;
