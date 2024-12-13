ed3d510d277042e4f66d50f0acd7fa5299f59d74
rtrtonLOL-Dev <115379532+rtrtonLOL-Dev@users.noreply.github.com>
2024-12-12 21:22:09 -0500

    Update files.ts
    Changed "H" to "h".

--- src/lib/util/files.ts
+++ src/lib/util/files.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 /**
  * Read an asset file with custom encoding.
  * @param assetPath The path to the asset that you want to read.
- * @param encoding THe encoding that you want to use while reading.
+ * @param encoding The encoding that you want to use while reading.
  * @returns {string}
  */
 export function readAssetFile(assetPath: string, encoding: BufferEncoding = 'utf-8'): string {
